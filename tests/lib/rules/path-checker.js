36497661d06dba2014e209f55a06053f660cbf91
Святослав Деев <109512200+xkochevnikx@users.noreply.github.com>
2023-07-07 16:46:23 +0300

    v0.0.7 add alias

--- tests/lib/rules/path-checker.js
+++ tests/lib/rules/path-checker.js
@@ -11,16 +11,46 @@ const rule = require('../../../lib/rules/path-checker'),
 // Tests
 //------------------------------------------------------------------------------
 
-const ruleTester = new RuleTester();
+const ruleTester = new RuleTester({
+    parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+});
 ruleTester.run('path-checker', rule, {
     valid: [
-        // give me some code that won't trigger a warning
+        {
+            filename:
+                'C:\\Users\\tim\\Desktop\\javascript\\production_project\\src\\entities\\Article',
+            code: "import { addCommentFormActions, addCommentFormReducer } from '../../model/slices/addCommentFormSlice'",
+            errors: [],
+        },
     ],
 
     invalid: [
         {
-            code: 'q',
-            errors: [{ message: 'Fill me in.', type: 'Me too' }],
+            filename:
+                'C:\\Users\\tim\\Desktop\\javascript\\production_project\\src\\entities\\Article',
+            code: "import { addCommentFormActions, addCommentFormReducer } from '@/entities/Article/model/slices/addCommentFormSlice'",
+            errors: [
+                {
+                    message:
+                        'В рамках одного слайса все пути должны быть относительными',
+                },
+            ],
+            options: [
+                {
+                    alias: '@',
+                },
+            ],
+        },
+        {
+            filename:
+                'C:\\Users\\tim\\Desktop\\javascript\\production_project\\src\\entities\\Article',
+            code: "import { addCommentFormActions, addCommentFormReducer } from 'entities/Article/model/slices/addCommentFormSlice'",
+            errors: [
+                {
+                    message:
+                        'В рамках одного слайса все пути должны быть относительными',
+                },
+            ],
         },
     ],
 });
